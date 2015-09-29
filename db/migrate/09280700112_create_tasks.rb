517f61e1c252c4dd876d89d87a1646b79a81860f
Gary Wong <garywong79@me.com>
2015-09-29 18:38:32 +0800

    Setup database

---
+++ db/migrate/09280700112_create_tasks.rb
@@ -0,0 +1,11 @@
+# require_relative 'config/application'
+
+class CreateTasks <ActiveRecord:: Migration 
+	def change
+		create_table :tasks do |t|
+			t.string :name
+			t.integer :complete
+		end
+	end
+end
+
