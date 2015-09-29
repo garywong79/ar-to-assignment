a82f934af4f7be08a7d053cd67e1727ba4803720
Gary Wong <garywong79@me.com>
2015-09-29 19:56:36 +0800

    created case statement

--- todo.rb
+++ todo.rb
@@ -1,5 +1,25 @@
 require_relative 'config/application'
 require 'byebug'
 
-byebug
-puts "Put your application code in #{File.expand_path(__FILE__)}"
+
+
+command = ARGV.shift
+string = ARGV.join(" ")
+
+case command
+when "list"
+	puts "list all tasks"
+when "new"
+	puts "create new task"
+when "complete"
+	puts "set task to complete"
+when "delete"
+	put "delete task"
+else
+	puts "Invalid Command."
+	puts "Available Command:"
+	puts "list - list all tasks"
+	puts "new [task name] - create new task"
+	puts "complete [id] - set task to complete"
+	puts "delete [id] - delete the task"
+end
